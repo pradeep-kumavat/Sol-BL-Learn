e438b681dfb020b8a0bc74ef7da75ad489bee223
pradeep-kumavat <22100BTCSE11679@svvvindore.onmicrosoft.com>
2025-02-14 21:48:12 +0530

    array and struct

--- contracts/Twiiter.sol
+++ contracts/Twiiter.sol
@@ -4,9 +4,32 @@ pragma solidity  ^0.8.26;
 
 contract Twitter{
 
-    mapping  (address => string) Tweet;
+    struct Tweet{
+        address author;
+        string content;
+        uint256 timestamp;
+        uint256 likes;
+    }
 
-    function addTweet(string memory _tweet) public {
-        Tweet[msg.sender] = _tweet;
+    mapping  (address => Tweet[] ) public tweets;
+
+    function createTweet(string memory _tweet) public {
+        Tweet memory newTweet = Tweet({
+            author: msg.sender,
+            content: _tweet,
+            timestamp: block.timestamp,
+            likes:0
+        });
+
+        tweets[msg.sender].push(newTweet);
+
+    }
+
+    function getTweet(address _owner,uint _i) public view returns (Tweet memory) {
+        return tweets[_owner][_i];
+    }
+
+    function getAllTweets(address _owner) public view returns (Tweet[] memory){
+        return tweets[_owner]; 
     }
 }
