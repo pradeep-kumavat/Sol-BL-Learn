e650946345626c0ba9d825de1507c6fac5beb8e7
pradeep-kumavat <22100BTCSE11679@svvvindore.onmicrosoft.com>
2025-02-16 14:20:19 +0530

    constructor & modifiers

--- contracts/Twiiter.sol
+++ contracts/Twiiter.sol
@@ -4,7 +4,10 @@ pragma solidity  ^0.8.26;
 
 contract Twitter{
 
+    uint16 public MAX_TWEET_LENGTH = 280;
+
     struct Tweet{
+        uint256 id;
         address author;
         string content;
         uint256 timestamp;
@@ -12,9 +15,27 @@ contract Twitter{
     }
 
     mapping  (address => Tweet[] ) public tweets;
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner,"You are not the owner");
+        _;
+    }
+
+    function changeTweetLength(uint16 newTweetLength) public onlyOwner {
+        MAX_TWEET_LENGTH = newTweetLength;
+    }
 
     function createTweet(string memory _tweet) public {
+        //conditionn statement
+        require(bytes(_tweet).length <= MAX_TWEET_LENGTH,"Tweet is too long!");
+
         Tweet memory newTweet = Tweet({
+            id:tweets[msg.sender].length,
             author: msg.sender,
             content: _tweet,
             timestamp: block.timestamp,
@@ -25,8 +46,19 @@ contract Twitter{
 
     }
 
-    function getTweet(address _owner,uint _i) public view returns (Tweet memory) {
-        return tweets[_owner][_i];
+    function likeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id,"Tweet does not exist");
+        tweets[author][id].likes++;
+    }
+
+    function unlikeTweet(address author, uint256 id) external {
+        require(tweets[author][id].id == id,"Tweet does not exist");
+        require(tweets[author][id].likes > 0,"Tweet has no likes");
+        tweets[author][id].likes--;
+    }
+
+    function getTweet(uint _i) public view returns (Tweet memory) {
+        return tweets[msg.sender][_i];
     }
 
     function getAllTweets(address _owner) public view returns (Tweet[] memory){
